3fb1796684c2f4880c65de37d8a79e53cf746943
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-03-26 15:30:16 +0100

    Add files via upload

---
+++ first js-task.js
@@ -0,0 +1,17 @@
+let name="Abdulmujeeb"
+console.log ("My name is: Abdulmujeeb")
+var courses=["html","CSS","Javascript","Design"]
+console.log ("My courses include: HTML,CSS,Javascript,Design")
+const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
